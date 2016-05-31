e2ae6e896099c09e24968b312af6c3d41690bb56
Samuli Seppänen <samuli@openvpn.net>
2016-05-31 14:32:14 +0300

    Ensure that build dependency packages do not cause resource conflicts
    Signed-off-by: Samuli Seppänen <samuli@openvpn.net>

--- manifests/buildenv.pp
+++ manifests/buildenv.pp
@@ -7,7 +7,5 @@ class openvpn::buildenv inherits openvpn::params {
 
     # We can't have quotes around the variable name, or Puppet will make the 
     # build dependency array a string, which will obviously fail.
-    package { $::openvpn::params::build_deps:
-        ensure => installed,
-    }
+    ensure_packages($::openvpn::params::build_deps)
 }
